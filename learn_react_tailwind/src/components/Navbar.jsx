d854c0962d7508382effdf53b5543e74d423fe87
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-03-05 22:48:41 +0200

    first

---
+++ learn_react_tailwind/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <>
+            <div className='flex justify-between items-center h-24 max-w-[1240] mx-auto px-4 text-white'>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React Tailwind Vite</h1>
+                <ul className='flex hidden'>
+                    <li className='p-4 '>Home</li>
+                    <li className='p-4 '>Company</li>
+                    <li className='p-4 '>resources</li>
+                    <li className='p-4 '>About</li>
+                    <li className='p-4 '>Contact</li>
+                </ul>
+                {/* open close menu button icons */}
+                <div onClick={handleNav}>
+                    {!nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20} />}
+                </div>
+            </div>
+            {/* mobile menu */}
+            <div className='bg-[#000300] fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full text-white'>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React Tailwind Vite</h1>
+                <ul className='pt-24 uppercase '>
+                    <li className='p-4 border-b border-gray-600'>Home</li>
+                    <li className='p-4 border-b border-gray-600'>Company</li>
+                    <li className='p-4 border-b border-gray-600'>resources</li>
+                    <li className='p-4 border-b border-gray-600'>About</li>
+                    <li className='p-4 '>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
